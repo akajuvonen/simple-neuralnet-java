39d7a7307964e512fdb9508239d417dd55294ec3
Antti Juvonen <akajuvonen@users.noreply.github.com>
2016-09-14 16:52:07 +0300

    Added template train and classify methods, updated main

--- NeuralNet.java
+++ NeuralNet.java
@@ -3,13 +3,24 @@
   */
 public class NeuralNet {
 
+    public void train() {
+        System.out.println("This is the train method");
+    }
+
+    public void classify() {
+        System.out.println("This is the classify method");
+    }
+
     /**
       * The main method.
       *
       * @param args the standard command line arguments
       */
     public static void main(String[] args) {
-        System.out.println("This will be a neural net");
+        // Doesn't do anything yet
+        NeuralNet net = new NeuralNet();
+        net.train();
+        net.classify();
     }
 
 }
