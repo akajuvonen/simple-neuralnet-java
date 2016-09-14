e0c18d604aad9075d36de03dc990a43142933d9b
Antti Juvonen <akajuvonen@users.noreply.github.com>
2016-09-14 09:18:29 +0300

    Added comments

--- NeuralNet.java
+++ NeuralNet.java
@@ -1,5 +1,13 @@
+/**
+  * This will be a neural network class.
+  */
 public class NeuralNet {
 
+    /**
+      * The main method.
+      *
+      * @param args the standard command line arguments
+      */
     public static void main(String[] args) {
         System.out.println("This will be a neural net");
     }
