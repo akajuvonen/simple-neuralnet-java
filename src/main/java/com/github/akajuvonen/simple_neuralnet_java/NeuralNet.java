7c3e8c00d39653d163faf322ed3db1be3d031513
Antti Juvonen <akajuvonen@users.noreply.github.com>
2016-09-20 15:35:34 +0300

    Added previously deleted placeholder methods

--- src/main/java/com/github/akajuvonen/simple_neuralnet_java/NeuralNet.java
+++ src/main/java/com/github/akajuvonen/simple_neuralnet_java/NeuralNet.java
@@ -4,10 +4,32 @@ package com.github.akajuvonen.simple_neuralnet_java;
  * NeuralNet class.
  *
  */
-public class NeuralNet 
+public class NeuralNet
 {
+  /**
+   * Trains the network using training data.
+   */
+  public void train() {
+      System.out.println("This is the train method");
+  }
+
+  /**
+   * Classifies new data.
+   */
+  public void classify() {
+      System.out.println("This is the classify method");
+  }
+    /**
+     * The main method.
+     *
+     * @param args the standard command line arguments
+     */
     public static void main( String[] args )
     {
         System.out.println( "This is a neural net." );
+        // Doesn't do anything yet
+        NeuralNet net = new NeuralNet();
+        net.train();
+        net.classify();
     }
 }
