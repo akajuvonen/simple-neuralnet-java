69d9b9e73f407c9b2fb86f11fb36e56469c524c2
Antti Juvonen <akajuvonen@users.noreply.github.com>
2016-09-20 15:28:41 +0300

    Renamed apptest to NeuralNetTest

--- src/test/java/com/github/akajuvonen/simple_neuralnet_java/NeuralNetTest.java
+++ src/test/java/com/github/akajuvonen/simple_neuralnet_java/NeuralNetTest.java
@@ -5,9 +5,9 @@ import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
 /**
- * Unit test for simple App.
+ * Unit test for neural network.
  */
-public class AppTest 
+public class NeuralNetTest 
     extends TestCase
 {
     /**
@@ -15,7 +15,7 @@ public class AppTest
      *
      * @param testName name of the test case
      */
-    public AppTest( String testName )
+    public NeuralNetTest( String testName )
     {
         super( testName );
     }
@@ -25,13 +25,13 @@ public class AppTest
      */
     public static Test suite()
     {
-        return new TestSuite( AppTest.class );
+        return new TestSuite( NeuralNetTest.class );
     }
 
     /**
      * Rigourous Test :-)
      */
-    public void testApp()
+    public void testNeuralNet()
     {
         assertTrue( true );
     }
