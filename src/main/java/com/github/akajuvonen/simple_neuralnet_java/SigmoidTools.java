c30fa0f4eb391112bc19e1a3ed344f2efb638688
Antti Juvonen <akajuvonen@users.noreply.github.com>
2016-09-27 18:28:10 +0300

    Added sigmoid tools as their own library

---
+++ src/main/java/com/github/akajuvonen/simple_neuralnet_java/SigmoidTools.java
@@ -0,0 +1,32 @@
+package com.github.akajuvonen.simple_neuralnet_java;
+
+/**
+ * Sigmoid and sigmoid derivative functions. Mainly useful for neural networks.
+ *
+ * @author Antti Juvonen
+ * @version 1.0
+ */
+public class SigmoidTools {
+  /**
+   * Sigmoid function. Returns 1 / (1+exp(-x)).
+   *
+   * @param x The input value
+   * @return Sigmoid function result
+   */
+  private double sigmoid(double x) {
+    return 1/(1+Math.exp(-x));
+  }
+
+  /**
+   * Sigmoid derivative function. x*(1-x).
+   * The values should already be calculated using
+   * the sigmoid method, i.e., the actual derivative
+   * is sigmoid(x)*(1-sigmoid(x))
+   *
+   * @param x The input value
+   * @return Sigmoid derivative function result
+   */
+  private double sigmoid_derivative(double x) {
+    return x*(1-x);
+  }
+}
