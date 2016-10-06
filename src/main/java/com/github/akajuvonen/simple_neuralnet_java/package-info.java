02a9b097bb83c7c43f64ee62eb01be9d63ac6278
Antti Juvonen <akajuvonen@users.noreply.github.com>
2016-10-06 10:15:42 +0300

    Added a package info file

---
+++ src/main/java/com/github/akajuvonen/simple_neuralnet_java/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Classes for a simple MPL neural network and related utilities.
+ * <p>
+ *
+ * @author Antti Juvonen
+ * @version 0.3
+ */
+package com.github.akajuvonen.simple_neuralnet_java;
