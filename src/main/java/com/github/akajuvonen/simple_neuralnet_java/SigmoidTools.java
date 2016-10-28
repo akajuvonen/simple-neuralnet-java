ff540fbdbf29c7e3e87872678c7b5c2037081442
Antti Juvonen <akajuvonen@users.noreply.github.com>
2016-10-28 08:18:18 +0300

    Added sigmoid deriv function for nested arrays

--- src/main/java/com/github/akajuvonen/simple_neuralnet_java/SigmoidTools.java
+++ src/main/java/com/github/akajuvonen/simple_neuralnet_java/SigmoidTools.java
@@ -56,4 +56,22 @@ public final class SigmoidTools {
         }
         return y;
     }
+
+    /**
+     * Sigmoid derivative for nested arrays.
+     *
+     * @param x The input array
+     * @return Sigmoid derivative function applied to all elements of x
+     */
+    public static double[][] sigmoidDerivative(final double[][] x) {
+        int n = x.length;
+        int m = x[0].length;
+        double[][] y = new double[n][m];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                y[i][j] = sigmoidDerivative(x[i][j]);
+            }
+        }
+        return y;
+    }
 }
