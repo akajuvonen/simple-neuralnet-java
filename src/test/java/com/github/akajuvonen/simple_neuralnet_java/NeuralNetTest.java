cd31c60537c1167afecbb6174b5ad61e66d09eaa
Antti Juvonen <akajuvonen@users.noreply.github.com>
2016-11-12 13:13:49 +0200

    Added neural net test

--- src/test/java/com/github/akajuvonen/simple_neuralnet_java/NeuralNetTest.java
+++ src/test/java/com/github/akajuvonen/simple_neuralnet_java/NeuralNetTest.java
@@ -1,7 +1,7 @@
 package com.github.akajuvonen.simple_neuralnet_java;
 
 import org.junit.Test;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Unit test for neural network.
@@ -15,8 +15,39 @@ public class NeuralNetTest {
      */
     @Test
     public void testNeuralNet() {
-        // Create a new net
-        //NeuralNet net = new NeuralNet(4,10000);
-        assertTrue(true);
+        // Input data for training
+        double[][] trainIn = {
+            {1.0, 0.0, 0.0},
+            {0.0, 0.0, 1.0},
+            {1.0, 1.0, 1.0},
+            {1.0, 1.0, 0.0},
+            {0.0, 1.0, 0.0},
+            {0.0, 0.0, 0.0}
+        };
+        // Training data outputs
+        double[][] trainOut = {
+            {1.0},
+            {0.0},
+            {1.0},
+            {1.0},
+            {0.0},
+            {0.0}
+        };
+        double[][] testIn = {
+            {1, 0, 1},
+            {0, 1, 0},
+            {1, 1, 1}
+        };
+        final int hiddenSize = 4;
+        final int maxIterations = 60000;
+        NeuralNet net = new NeuralNet(hiddenSize, maxIterations,
+            trainIn, trainOut);
+        double[][] result = net.classify(testIn);
+        // Expected results
+        double[] expected = {1, 0, 1};
+        for (int i = 0; i < result.length; i++) {
+            // Should be rounded to 1 0 1
+            assertEquals(result[i][0], expected[i], 0.1);
+        }
     }
 }
