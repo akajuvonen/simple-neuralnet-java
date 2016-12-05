66dae58d759dd62c02c34fa09103011801f11108
Antti Juvonen <akajuvonen@users.noreply.github.com>
2016-12-05 08:18:59 +0200

    Corrected scalar multiplication test

--- src/test/java/com/github/akajuvonen/simple_neuralnet_java/MatrixToolsTest.java
+++ src/test/java/com/github/akajuvonen/simple_neuralnet_java/MatrixToolsTest.java
@@ -147,14 +147,12 @@ public class MatrixToolsTest {
             {1, 2, 3},
             {4, 5, 6}
         };
-        double[][] b = {
-            {2}
-        };
+        double b = 2;
         double[][] expected = {
             {2, 4, 6},
             {8, 10 , 12}
         };
-        double[][] actual = MatrixTools.multiply(a, b);
+        double[][] actual = MatrixTools.multiplyElementwise(a, b);
         for (int i = 0; i < expected.length; i++) {
             assertArrayEquals(expected[i], actual[i], 0.0);
         }
