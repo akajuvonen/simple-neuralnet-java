9090221456c47cb7529e6e7303b7de287583f28d
Antti Juvonen <akajuvonen@users.noreply.github.com>
2016-12-15 09:09:19 +0200

    Added import for neuralnet in iris test

--- src/main/java/com/github/akajuvonen/simple_neuralnet_java/IrisAnalysis.java
+++ src/main/java/com/github/akajuvonen/simple_neuralnet_java/IrisAnalysis.java
@@ -1,5 +1,7 @@
 package com.github.akajuvonen.simple_neuralnet_java;
 
+import com.github.akajuvonen.simple_neuralnet_java.NeuralNet;
+
 public class IrisAnalysis {
     public IrisAnalysis() {
         //TODO
