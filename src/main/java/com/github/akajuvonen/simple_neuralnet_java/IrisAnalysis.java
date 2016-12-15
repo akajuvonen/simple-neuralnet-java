f2ba5c91f7377102ff6143234c44388b72a8ba28
Antti Juvonen <akajuvonen@users.noreply.github.com>
2016-12-15 09:10:48 +0200

    Added javadoc, author and version for iris class

--- src/main/java/com/github/akajuvonen/simple_neuralnet_java/IrisAnalysis.java
+++ src/main/java/com/github/akajuvonen/simple_neuralnet_java/IrisAnalysis.java
@@ -2,6 +2,12 @@ package com.github.akajuvonen.simple_neuralnet_java;
 
 import com.github.akajuvonen.simple_neuralnet_java.NeuralNet;
 
+/**
+ * Iris data analysis test class.
+ *
+ * @author Antti Juvonen
+ * @version 0.1
+ */
 public class IrisAnalysis {
     public IrisAnalysis() {
         //TODO
