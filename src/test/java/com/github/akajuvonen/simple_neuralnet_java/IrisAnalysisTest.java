205d232ced612e064c0fce64b7bbd7279562ab07
Antti Juvonen <akajuvonen@users.noreply.github.com>
2016-12-21 08:21:26 +0200

    Added iris data analysis test placeholder

---
+++ src/test/java/com/github/akajuvonen/simple_neuralnet_java/IrisAnalysisTest.java
@@ -0,0 +1,20 @@
+package com.github.akajuvonen.simple_neuralnet_java;
+
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Unit tests for iris data analysis.
+ *
+ * @author Antti Juvonen
+ * @version 0.1
+ */
+public class IrisAnalysisTest {
+    /**
+     * Test smth.
+     */
+    @Test
+    public void testSmth() {
+        assertTrue(true);
+    }
+}
