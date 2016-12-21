6f98d5902889d6ab91b033dd625d321143160700
Antti Juvonen <akajuvonen@users.noreply.github.com>
2016-12-21 08:31:56 +0200

    Added javadocs

--- src/main/java/com/github/akajuvonen/simple_neuralnet_java/IrisAnalysis.java
+++ src/main/java/com/github/akajuvonen/simple_neuralnet_java/IrisAnalysis.java
@@ -9,14 +9,29 @@ import com.github.akajuvonen.simple_neuralnet_java.NeuralNet;
  * @version 0.1
  */
 public class IrisAnalysis {
+    /**
+     * Iris data analysis constructor.
+     *
+     * @param filename Path to iris data csv file
+     */
     public IrisAnalysis(final String filename) {
         parseCSV(filename);
     }
 
+    /**
+     * Iris data CSV parser.
+     *
+     * @param filename Path to iris data csv file
+     */
     private void parseCSV(final String filename) {
         //
     }
 
+    /**
+     * The main method.
+     *
+     * @param args Command line arguments
+     */
     public static void main(final String[] args) {
         String filename = "data/iris.data";
         IrisAnalysis iris = new IrisAnalysis(filename);
