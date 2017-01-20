e319e4818f420a24fc82710dac69295e57b0227d
Antti Juvonen <akajuvonen@users.noreply.github.com>
2017-01-20 09:09:28 +0200

    The csv parser now parses measurements to string, then converts to double and inserts into an array. TODO: Parse the training data outputs also (flower names)

--- src/main/java/com/github/akajuvonen/simple_neuralnet_java/IrisAnalysis.java
+++ src/main/java/com/github/akajuvonen/simple_neuralnet_java/IrisAnalysis.java
@@ -3,6 +3,7 @@ package com.github.akajuvonen.simple_neuralnet_java;
 import java.io.IOException;
 import java.io.FileReader;
 import java.io.BufferedReader;
+import java.util.Arrays;
 
 /**
  * Iris data analysis class.
@@ -50,14 +51,22 @@ public class IrisAnalysis {
     private void parseCSV(final String filename) {
         String line;
         String[] splitline;
+        double[][] parsed = new double[150][5];
         try {
             FileReader fr = new FileReader(filename);
             BufferedReader br = new BufferedReader(fr);
+            int i = 0;
             // For now just prints line by line
             for (line = br.readLine(); line != null;
                  line = br.readLine()) {
                 // Split the line into tokens using the separator
                 splitline = line.split(separator);
+                /** Parse the measured values to string, then convert to double
+                and insert to parsed output array. */
+                String[] inputString = Arrays.copyOfRange(splitline, 0, 4);
+                for (int j = 0; j < inputString.length; j++) {
+                    parsed[i][j] = Double.parseDouble(inputString[j]);
+                }
             }
         } catch (IOException e) {
             // IO Exception includes errors if file not found, as well as
