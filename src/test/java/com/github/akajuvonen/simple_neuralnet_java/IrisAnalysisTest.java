26aa8d6e361d1bb9d47833fcba80304033a8c5a9
Antti Juvonen <akajuvonen@users.noreply.github.com>
2017-02-09 08:07:55 +0200

    Iris analysis csv reader test now prints some test line to a file called test.data

--- src/test/java/com/github/akajuvonen/simple_neuralnet_java/IrisAnalysisTest.java
+++ src/test/java/com/github/akajuvonen/simple_neuralnet_java/IrisAnalysisTest.java
@@ -3,6 +3,9 @@ package com.github.akajuvonen.simple_neuralnet_java;
 import org.junit.Test;
 import static org.junit.Assert.assertTrue;
 
+import java.io.PrintWriter;
+import java.io.IOException;
+
 /**
  * Unit tests for iris data analysis.
  *
@@ -15,6 +18,16 @@ public class IrisAnalysisTest {
      */
     @Test
     public void testCSVParser() {
+        // Print test lines to file
+        try{
+            PrintWriter writer = new PrintWriter("test.data", "UTF-8");
+            writer.println("1.0,2.0,3.0,4.0,Iris-setosa");
+            writer.println("5.0,6.0,7.0,8.0,Iris-versicolor");
+            writer.println("9.0,10.0,11.0,12.0,Iris-virginica");
+            writer.close();
+        } catch (IOException e) {
+            System.out.println("Can't read test file");
+}
         assertTrue(true);
     }
 
