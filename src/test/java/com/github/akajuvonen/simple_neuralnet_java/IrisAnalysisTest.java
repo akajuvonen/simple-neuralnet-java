0f6e2e8179393957a60254ca5849bba47f7460d1
Antti Juvonen <akajuvonen@users.noreply.github.com>
2017-02-12 10:26:35 +0200

    Removed unneeded tests for private methods, can't test them.

--- src/test/java/com/github/akajuvonen/simple_neuralnet_java/IrisAnalysisTest.java
+++ src/test/java/com/github/akajuvonen/simple_neuralnet_java/IrisAnalysisTest.java
@@ -14,36 +14,11 @@ import java.io.IOException;
  */
 public class IrisAnalysisTest {
     /**
-     * Test CSV parsing.
+     * Test that iris analysis gives correct results.
+     * Note that we do not not (can't) test private methods inside the class.
      */
     @Test
-    public void testCSVParser() {
-        // Print test lines to file
-        try{
-            PrintWriter writer = new PrintWriter("test.data", "UTF-8");
-            writer.println("1.0,2.0,3.0,4.0,Iris-setosa");
-            writer.println("5.0,6.0,7.0,8.0,Iris-versicolor");
-            writer.println("9.0,10.0,11.0,12.0,Iris-virginica");
-            writer.close();
-        } catch (IOException e) {
-            System.out.println("Can't read test.data");
-        }
-        assertTrue(true);
-    }
-
-    /**
-     * Test data shuffling.
-     */
-    @Test
-    public void testDataShuffling() {
-        assertTrue(true);
-    }
-
-    /**
-     * Test data splitting.
-     */
-    @Test
-    public void testDataSplitting() {
+    public void testIrisAnalysis() {
         assertTrue(true);
     }
 }
