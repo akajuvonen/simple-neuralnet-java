9ade6b471784ccad875fd6ea231dad0c65d76088
Antti Juvonen <akajuvonen@users.noreply.github.com>
2017-02-17 08:29:34 +0200

    Iris test now calculates correct classifications

--- src/test/java/com/github/akajuvonen/simple_neuralnet_java/IrisAnalysisTest.java
+++ src/test/java/com/github/akajuvonen/simple_neuralnet_java/IrisAnalysisTest.java
@@ -1,7 +1,7 @@
 package com.github.akajuvonen.simple_neuralnet_java;
 
 import org.junit.Test;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 
 import java.io.PrintWriter;
 import java.io.IOException;
@@ -10,7 +10,7 @@ import java.io.IOException;
  * Unit tests for iris data analysis.
  *
  * @author Antti Juvonen
- * @version 0.1
+ * @version 0.5
  */
 public class IrisAnalysisTest {
     /**
@@ -22,5 +22,20 @@ public class IrisAnalysisTest {
         String filename = "data/iris.data";
         IrisAnalysis iris = new IrisAnalysis(filename, ",");
         double[][] result = iris.classify();
+        // For some reason we have to call getter like this to get testOut
+        double[][] testOut = iris.getTestOut();
+        // Correct classifications
+        int correctCount = 0;
+        // Check how many classfications are correct
+        for (int i = 0; i < testOut.length; i++) {
+            for (int j = 0; j < result[i].length; j++) {
+                int expected = (int)Math.round(testOut[i][j]);
+                int actual = (int)Math.round(result[i][j]);
+                if (expected == actual) {
+                    correctCount++;
+                }
+            }
+        }
+        System.out.println(correctCount);
     }
 }
