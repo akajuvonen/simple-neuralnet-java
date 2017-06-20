fa98a5182fc820be96c5c7462d190b02a31ed78a
Antti Juvonen <akajuvonen@users.noreply.github.com>
2017-06-20 09:51:41 +0300

    Added expected and actual iris results as variables, for readability and further use

--- src/main/java/com/github/akajuvonen/simple_neuralnet_java/IrisAnalysis.java
+++ src/main/java/com/github/akajuvonen/simple_neuralnet_java/IrisAnalysis.java
@@ -210,15 +210,17 @@ public class IrisAnalysis {
         double[][] result = iris.classify();
         // Print the result and expected actual result from test data
         for (int i = 0; i < iris.testOut.length; i++) {
+            int actual = Math.round(result[i][j]);
+            int expected = Math.round(iris.testOut[i][j]);
             // Print the line number
             System.out.println(i + 1);
             System.out.println("Classification result:");
             for (int j = 0; j < result[i].length; j++) {
-                System.out.println(Math.round(result[i][j]));
+                System.out.println(actual;
             }
             System.out.println("Expected result:");
             for (int j = 0; j < iris.testOut[i].length; j++) {
-                System.out.println(Math.round(iris.testOut[i][j]));
+                System.out.println(expected);
             }
             System.out.println("-----");
         }
